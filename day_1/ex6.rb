a501e37a19f6d6e0f5c744e054b20b20c48f0d57
Andrew James <ajames20@gmail.com>
2018-08-18 20:37:21 -0600

    Add ex6.rb

---
+++ day_1/ex6.rb
@@ -0,0 +1,36 @@
+# assign types_of_people the value 10
+types_of_people = 10
+# assign x a string and interpolate types_of_people
+x = "There are #{types_of_people} types of people."
+# assign binary the string of binary
+binary = 'binary'
+# assign do_not the string don'
+do_not = "don't"
+# assign y to a string and interpolate binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+
+# print x
+puts x
+# print y
+puts y
+
+# puts the string and interpolate x
+puts "I said: #{x}."
+# puts the string and interpolate y
+puts "I also said: '#{y}'."
+
+# assign hilarious a boolean of false
+hilarious = false
+# assign joke_evaluation a string and interpolate hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# print joke_evaluation
+puts joke_evaluation
+
+# assign w a string
+w = 'This is the left side of...'
+# assign e a string
+e = 'a string with a right side.'
+
+# the + operator is concatenating w and e
+puts w + e
