14df5b68c33c574f0eb49e04a0fdc206a34a6427
Andrew James <ajames20@gmail.com>
2018-08-18 20:40:41 -0600

    Add ex7.rb

---
+++ day_1/ex7.rb
@@ -0,0 +1,10 @@
+print 'How old are you? '
+age = gets.chomp
+print 'How tall are you? '
+height = gets.chomp
+print 'How much do you weigh? '
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+# gets is method we can call to wait to obtain user input
+# chomp is a method that can be called on a string to remove carriage returns
